b8b183a4b3566c1893dd3e0fc9219d4a4faef418
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-25 17:40:57 +0200

    devises => devices

--- app/models/concerns/statsable.rb
+++ app/models/concerns/statsable.rb
@@ -6,7 +6,7 @@ module Statsable
   included do
     field :lo, as: :loads, type: Hash # { a(all): 12, w(website): 3, e(external): 9 }
     field :st, as: :starts, type: Hash # { a(all): 12, w(website): 3, e(external): 9 }
-    field :de, as: :devises, type: Hash # { a(all): { "d" => 2, "m" => 4 }, w(website): ... }
+    field :de, as: :devices, type: Hash # { a(all): { "d" => 2, "m" => 4 }, w(website): ... }
     field :co, as: :countries, type: Hash # { a(all): { "usa" => 2, "fra" => 4, ... }, w(website): ... }
     field :bp, as: :browser_and_platform, type: Hash # { a(all): { "saf-win" => 2, "saf-osx" => 4, ... }, w(website): ... }
   end
--- spec/models/concerns/statsable_spec.rb
+++ spec/models/concerns/statsable_spec.rb
@@ -12,7 +12,7 @@ describe Statsable do
 
   it { should have_field(:lo).with_alias(:loads).of_type(Hash) }
   it { should have_field(:st).with_alias(:starts).of_type(Hash) }
-  it { should have_field(:de).with_alias(:devises).of_type(Hash) }
+  it { should have_field(:de).with_alias(:devices).of_type(Hash) }
   it { should have_field(:co).with_alias(:countries).of_type(Hash) }
   it { should have_field(:bp).with_alias(:browser_and_platform).of_type(Hash) }
 end
