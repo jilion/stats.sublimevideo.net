5da111c8739e25e8574612a1bb35e96065c71659
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-30 17:09:57 +0200

    Add all/website/external methods on Statsable module

--- app/models/concerns/statsable.rb
+++ app/models/concerns/statsable.rb
@@ -4,10 +4,30 @@ module Statsable
   extend ActiveSupport::Concern
 
   included do
-    field :lo, as: :loads, type: Hash # { a(all): 12, w(website): 3, e(external): 9 }
-    field :st, as: :starts, type: Hash # { a(all): 12, w(website): 3, e(external): 9 }
-    field :de, as: :devices, type: Hash # { a(all): { "d" => 2, "m" => 4 }, w(website): ... }
-    field :co, as: :countries, type: Hash # { a(all): { "usa" => 2, "fra" => 4, ... }, w(website): ... }
-    field :bp, as: :browser_and_platform, type: Hash # { a(all): { "saf-win" => 2, "saf-osx" => 4, ... }, w(website): ... }
+    field :lo, as: :loads, type: Hash # { w(website): 3, e(external): 9 }
+    field :st, as: :starts, type: Hash # { w(website): 3, e(external): 9 }
+    field :de, as: :devices, type: Hash # { w(website): { "d" => 2, "m" => 4 }, e(external): ... }
+    field :co, as: :countries, type: Hash # { w(website): { "us" => 2, "fr" => 4, ... }, e(external): ... }
+    field :bp, as: :browser_and_platform, type: Hash # { w(website): { "saf-win" => 2, "saf-osx" => 4, ... }, e(external): ... }
+  end
+
+  def all(field)
+    w, e = website(field), external(field)
+    case field_default(field)
+    when Integer then w + e
+    when Hash then w.merge(e) { |k, v1, v2| v1 + v2 }
+    end
+  end
+
+  def website(field)
+    send(field)['w'] || field_default(field)
+  end
+
+  def external(field)
+    send(field)['e'] || field_default(field)
+  end
+
+  def field_default(field)
+    field.in?(%i[loads starts]) ? 0 : {}
   end
 end
--- spec/models/concerns/statsable_spec.rb
+++ spec/models/concerns/statsable_spec.rb
@@ -15,4 +15,57 @@ describe Statsable do
   it { should have_field(:de).with_alias(:devices).of_type(Hash) }
   it { should have_field(:co).with_alias(:countries).of_type(Hash) }
   it { should have_field(:bp).with_alias(:browser_and_platform).of_type(Hash) }
+
+  context "with a model with stats" do
+    let(:model) { StatsableModel.new(
+      loads: { 'w' => 2, 'e' => 4 },
+      starts: { 'e' => 3 },
+      devices: { 'w' => { 'm' => 3, 'd' => 5 }, 'e' => { 'm' => 5, 'd' => 10 } },
+      countries: { 'e' => { 'ch' => 5, 'fr' => 10 } }
+    ) }
+
+    describe "#website" do
+      it "returns website loads" do
+        model.website(:loads).should eq 2
+      end
+      it "returns website starts" do
+        model.website(:starts).should eq 0
+      end
+      it "returns website devices" do
+        model.website(:devices).should eq({ 'm' => 3, 'd' => 5 })
+      end
+      it "returns website countries" do
+        model.website(:countries).should eq({})
+      end
+    end
+    describe "#external" do
+      it "returns external loads" do
+        model.external(:loads).should eq 4
+      end
+      it "returns website starts" do
+        model.external(:starts).should eq 3
+      end
+      it "returns external devices" do
+        model.external(:devices).should eq({ 'm' => 5, 'd' => 10 })
+      end
+      it "returns website countries" do
+        model.external(:countries).should eq({ 'ch' => 5, 'fr' => 10 })
+      end
+    end
+    describe "#all" do
+      it "returns all loads" do
+        model.all(:loads).should eq 6
+      end
+      it "returns all starts" do
+        model.all(:starts).should eq 3
+      end
+      it "returns all devices" do
+        model.all(:devices).should eq({ 'm' => 8, 'd' => 15 })
+      end
+      it "returns all countries" do
+        model.all(:countries).should eq({ 'ch' => 5, 'fr' => 10 })
+      end
+    end
+  end
+
 end
