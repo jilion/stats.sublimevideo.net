92f134f978482f46adb0e84482ac828c9fa710cd
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-09-10 21:25:53 +0200

    Remove duplication
    @rymain please don't remove my 4.0 score :)

--- app/controllers/private_api/video_stats_controller.rb
+++ app/controllers/private_api/video_stats_controller.rb
@@ -2,15 +2,19 @@ class PrivateApi::VideoStatsController < SublimeVideoPrivateApiController
 
   # GET /private_api/sites/:site_token/video_stats/:id
   def show
-    stats = VideoStat.last_hours_stats({ site_token: params[:site_token], video_uid: params[:id] },
-                                        params[:hours].to_i)
+    stats = VideoStat.last_hours_stats(_video_args, params[:hours].to_i)
     respond_with(stats: stats)
   end
 
   # GET /private_api/sites/:site_token/video_stats/:id/last_days_starts
   def last_days_starts
-    starts = VideoStat.last_days_starts({ site_token: params[:site_token], video_uid: params[:id] },
-                                        params[:days].to_i)
-    respond_with(starts: starts)
+    stats = VideoStat.last_days_starts(_video_args, params[:days].to_i)
+    respond_with(starts: stats)
+  end
+
+  private
+
+  def _video_args
+    { site_token: params[:site_token], video_uid: params[:id] }
   end
 end
