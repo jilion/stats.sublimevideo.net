5e9fa8833bf38142dfae57a58e7dc2ad49f335f0
Rémy Coutable <remy@jilion.com>
2013-09-24 11:45:26 +0200

    Returns the last 3 plays instead of 5

--- app/controllers/private_api/last_plays_controller.rb
+++ app/controllers/private_api/last_plays_controller.rb
@@ -3,7 +3,7 @@ class PrivateApi::LastPlaysController < SublimeVideoPrivateApiController
   # GET /private_api/sites/:site_token/last_plays
   # GET /private_api/sites/:site_token/videos/:video_uid/last_plays
   def index
-    plays = LastPlay.where(_args).desc(:time).limit(5)
+    plays = LastPlay.where(_args).desc(:time).limit(3)
     plays = plays.since(params[:since]) if params.has_key?(:since)
 
     if stale?(etag: _args, last_modified: plays.max(:time))
